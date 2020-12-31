25b77bdc15ac5156f3e26271a6c023376f7de81a
changjunhang <l>
2020-12-31 18:59:19 +0800

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store';
 import Home from '../views/Home.vue';
+import userRoute from './module/user';
 
 Vue.use(VueRouter);
 
@@ -18,12 +20,29 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  ...userRoute,
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth) { // 判断是否登录
+    // 判断用户是否登录
+    if (store.state.userModule.token) {
+      // 判断token的有效性，比如有没过期，需要后台发放token的时候，带上token的有效期
+      // 如果token无效，请求token
+      next();
+    } else {
+      // 跳转登录
+      router.push({ name: 'login' });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
